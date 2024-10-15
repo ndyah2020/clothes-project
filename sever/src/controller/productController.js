31b5acb74b7839bd64f2dcf93f80753960a02365
ndyah2020 <nguyenduyanhh2020@gmail.com>
2024-10-15 19:28:45 +0700

    Comment Source

--- sever/src/controller/productController.js
+++ sever/src/controller/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../model/Product')
 
 class ProductController {
+    //Hiển thị sản phẩm method: get - http://localhost:3001/category/show
     show(req, res) {
         Product.find({})
             .then(products => {
@@ -11,11 +12,13 @@ class ProductController {
                 res.status(500).json({ message: 'Error retrieving products', error });
             });
     }
-
+    //Tạo sản phẩm mới method: post - /category/create
     create(req, res) {
         const formData = req.body
+        //Kiểm tra sản phẩm có sku hoặc tên giống trong database không
         Product.findOne({ $or: [{ sku: formData.sku }, { name: formData.name }] })
         .then(existingProduct => {
+            //nếu có trả về sản phẩm đó đồng thời kiểm tra giống nhau thì in ra 
             if(existingProduct){
                 if(existingProduct.sku === formData.sku){
                     return res.status(400).json({ message: 'SKU này đã tồn tại' });
@@ -24,6 +27,7 @@ class ProductController {
                     return res.status(400).json({ message: 'Tên đã tồn tại' });
                 }
             }
+            //Không có thì tạo đối tượng lưu vào database  
             const product = new Product(formData)
             return product.save()
         })
@@ -34,7 +38,7 @@ class ProductController {
         })
         .catch(error => res.status(500).json({ message: 'Thêm sản phẩm thất bại', error }))
     }
-    
+    //Chỉnh sửa sản phẩm method: put  - http://localhost:3001/category/id
     update(req, res) {
         const productId = req.params.id
         const updatedData = req.body
@@ -45,7 +49,7 @@ class ProductController {
             res.status(500).json({ message: 'Error retrieving products', error });
         });
     }
-
+    //Tìm kiếm sản phẩm theo tên method: get http://localhost:3001/category/search
     search(req, res) {
         Product.find(req.params.name)
         .then((product) => {
