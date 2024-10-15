b6c09df5b74c5aa71a6ded143cd0ee6060ed5317
ndyah2020 <nguyenduyanhh2020@gmail.com>
2024-10-15 15:28:00 +0700

    show-create-product-update

--- sever/src/controller/productController.js
+++ sever/src/controller/productController.js
@@ -11,14 +11,18 @@ class ProductController {
                 res.status(500).json({ message: 'Error retrieving products', error });
             });
     }
+
     create(req, res) {
         const formData = req.body
-        
+
         const product = new Product(formData)
         product.save()
         .then(() => res.send('Them roi do cu'))
-        .catch(next)
+        .catch(error => {
+            res.status(500).json({ message: 'Error retrieving products', error });
+        });
     }
+
 }
 
 module.exports = new ProductController();
