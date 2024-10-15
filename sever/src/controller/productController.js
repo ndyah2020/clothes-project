a27a35db0027cf5a2efb1e7e94dd16b7cd83c19e
ndyah2020 <nguyenduyanhh2020@gmail.com>
2024-10-15 18:22:14 +0700

    create,update,show,search

--- sever/src/controller/productController.js
+++ sever/src/controller/productController.js
@@ -14,15 +14,47 @@ class ProductController {
 
     create(req, res) {
         const formData = req.body
-
-        const product = new Product(formData)
-        product.save()
-        .then(() => res.send('Them roi do cu'))
+        Product.findOne({ $or: [{ sku: formData.sku }, { name: formData.name }] })
+        .then(existingProduct => {
+            if(existingProduct){
+                if(existingProduct.sku === formData.sku){
+                    return res.status(400).json({ message: 'SKU này đã tồn tại' });
+                }
+                if(existingProduct.name === formData.name){
+                    return res.status(400).json({ message: 'Tên đã tồn tại' });
+                }
+            }
+            const product = new Product(formData)
+            return product.save()
+        })
+        .then(saveProduct =>{
+            if(saveProduct){
+                return res.status(200).json(saveProduct)
+            }
+        })
+        .catch(error => res.status(500).json({ message: 'Thêm sản phẩm thất bại', error }))
+    }
+    
+    update(req, res) {
+        const productId = req.params.id
+        const updatedData = req.body
+        
+        Product.findByIdAndUpdate(productId, updatedData)
+        .then(() => res.json({updatedData }))
         .catch(error => {
             res.status(500).json({ message: 'Error retrieving products', error });
         });
     }
 
+    search(req, res) {
+        Product.find(req.params.name)
+        .then((product) => {
+            res.json(product)
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Error retrieving products', error });
+        });
+    }
 }
 
 module.exports = new ProductController();
--- sever/src/model/Product.js
+++ sever/src/model/Product.js
@@ -2,10 +2,17 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const Product = new Schema({
+    sku : { type: String },
     name: { type: String },
     color: { type: String },
     size: { type: String },
-    price: { type: String }
+    quantity: { type: String},
+    price: { type: String },
+    status: {
+            type: String,
+            enum: ["active", "inactive", "suspended"],
+            default: "active",
+        },
 }, {
     timestamps: true
 });
