e4236e1e8b1881061599a1fdaf40c1d57e5a3efc
Thanh Yarn <93782548+thanhyarn@users.noreply.github.com>
2024-10-20 22:57:09 +0700

    Add files via upload

--- sever/src/routes/index.js
+++ sever/src/routes/index.js
@@ -1,9 +1,9 @@
-const categoryRouter = require("./category");
 const userRouter = require("./user");
+const productRouter = require("./product");
 
 function route(app) {
-  app.use("/category", categoryRouter);
   app.use("/user", userRouter);
+  app.use("/product", productRouter);
 }
 
 module.exports = route;
---
+++ sever/src/routes/product.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const productController = require("../controller/productController");
+const multer = require("multer");
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+// Lấy tất cả sản phẩm
+router.get("/get-products", productController.getProducts);
+
+// Lấy thông tin chi tiết của một sản phẩm theo ID
+router.get("/get-product/:id", productController.getProductById);
+
+// Tạo mới sản phẩm
+router.post(
+  "/create-product",
+  upload.array("images"),
+  productController.createProduct
+);
+
+// Cập nhật thông tin sản phẩm theo ID
+router.put(
+  "/update-product/:id",
+  upload.array("images"),
+  productController.updateProduct
+);
+
+// Xóa một sản phẩm theo ID
+router.delete("/delete-product/:id", productController.deleteProduct);
+
+// Lấy sản phẩm theo danh mục (ví dụ: category="shirt")
+router.get(
+  "/get-products/category/:category",
+  productController.getProductsByCategory
+);
+
+// Lấy sản phẩm theo trạng thái (ví dụ: in_stock, out_of_stock)
+router.get(
+  "/get-products/status/:status",
+  productController.getProductsByStatus
+);
+
+module.exports = router;
